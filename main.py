2ba9e2aa33c481a72dfc7f81b140c16c69ca9419
tokyo <tokyolabs@protonmail.com>
2024-01-15 15:54:28 -0500

    wip

--- main.py
+++ main.py
@@ -12,5 +12,23 @@ application.resizable(False, False)
 # window title
 application.title("My Restaurant - Invoicing System")
 
+# window background color
+# https://www.plus2net.com/python/tkinter-colors.php
+application.config(bg="antiquewhite4")
+
+# top panel
+top_panel = Frame(application, bd=1, relief=FLAT)
+top_panel.pack(side=TOP)
+
+# title tag
+title_tag = Label(
+    top_panel,
+    text="Invoicing System",
+    fg="azure4",
+    font=("Dosis", 58),
+    bg="burlywood",
+    width=27,
+)
+
 # prevent window from closing
 application.mainloop()
