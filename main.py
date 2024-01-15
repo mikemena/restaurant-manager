936fd0b85cff278bd5a59e6c2078ec3aeede2117
tokyo <tokyolabs@protonmail.com>
2024-01-15 14:18:54 -0500

    wip

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ application = Tk()
 # Window size
 application.geometry("1020x630+0+0")
 
+# prevent from maximiaing
+application.resizable(False, False)
+
+# window title
+application.title("My Restaurant - Invoicing System")
+
 # prevent window from closing
 application.mainloop()
