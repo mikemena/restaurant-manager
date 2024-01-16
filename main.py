bdf722618f7347441407038575e0cd24a329463c
tokyo <tokyolabs@protonmail.com>
2024-01-16 12:16:27 -0500

    setup right panels

--- main.py
+++ main.py
@@ -39,5 +39,44 @@ left_panel.pack(side=LEFT)
 cost_panel = Frame(application, bd=1, relief=FLAT)
 cost_panel.pack(side=BOTTOM)
 
+# food panel
+food_panel = LabelFrame(
+    left_panel, text="Food", font=("Dosis", 19, "bold"), bd=0, relief=FLAT, fg="azure4"
+)
+food_panel.pack(side=LEFT)
+
+# drink panel
+drink_panel = LabelFrame(
+    left_panel, text="Drink", font=("Dosis", 19, "bold"), bd=0, relief=FLAT, fg="azure4"
+)
+drink_panel.pack(side=LEFT)
+
+# dessert panel
+dessert_panel = LabelFrame(
+    left_panel,
+    text="Dessert",
+    font=("Dosis", 19, "bold"),
+    bd=0,
+    relief=FLAT,
+    fg="azure4",
+)
+dessert_panel.pack(side=LEFT)
+
+# right panel
+right_panel = Frame(application, bd=1, relief=FLAT)
+right_panel.pack(side=RIGHT)
+
+# calculator panel
+calculator_panel = Frame(right_panel, bd=0, relief=FLAT, bg="azure4")
+calculator_panel.pack()
+
+# invoice panel
+invoice_panel = Frame(right_panel, bd=0, relief=FLAT, bg="azure4")
+invoice_panel.pack()
+
+# buttons panel
+buttons_panel = Frame(right_panel, bd=0, relief=FLAT, bg="azure4")
+buttons_panel.pack()
+
 # prevent window from closing
 application.mainloop()
